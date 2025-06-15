d08f888852fdb285b8690495408062334e6a40e2
Nasriddinov Ubaydullox <nasriddinovadildora45@gmail.com>
2025-06-15 17:20:08 +0500

    Commit-2

--- TrainingTask_V2/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ TrainingTask_V2/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d8195b3eb1ca66135d10f9305724a40ff38295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
